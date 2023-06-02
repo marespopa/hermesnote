37a7484d995ee59083e989339d41cc4f10c371b0
smoothwizz <popa.mares@gmail.com>
2023-06-02 15:24:32 +0300

    Update app types

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
-import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/global.scss";
+import { Session } from "next-auth/core/types";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
