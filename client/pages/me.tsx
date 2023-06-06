c8433a836a57bfc361b0a29eb506e1d2b53f1a7a
smoothwizz <popa.mares@gmail.com>
2023-06-06 23:00:36 +0300

    chore: remove middleware

--- client/pages/me.tsx
+++ client/pages/me.tsx
@@ -1,25 +1,30 @@
 import { useSession } from "next-auth/react";
 import Layout from "../components/layout";
+import AccessDenied from "@/components/access-denied";
 
 export default function MePage() {
   const { data } = useSession();
 
-  return (
-    <Layout>
-      <div className="profile">
-        <h2 className="profile__title">
-          Hi
-          <span className="profile__username">
-            <img
-              className="w-8 h-8 rounded-full"
-              src={data?.user?.image || ""}
-              alt="User Profile Pic"
-            />
-            {data?.user?.name}!
-          </span>
-        </h2>
-        <p className="profile__email">Your email is {data?.user?.email}</p>
-      </div>
-    </Layout>
-  );
+  if (data) {
+    return (
+      <Layout>
+        <div className="profile">
+          <h2 className="profile__title">
+            Hi
+            <span className="profile__username">
+              <img
+                className="w-8 h-8 rounded-full"
+                src={data?.user?.image || ""}
+                alt="User Profile Pic"
+              />
+              {data?.user?.name}!
+            </span>
+          </h2>
+          <p className="profile__email">Your email is {data?.user?.email}</p>
+        </div>
+      </Layout>
+    );
+  }
+
+  return <AccessDenied />;
 }
