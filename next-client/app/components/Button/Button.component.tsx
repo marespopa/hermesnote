e3f166a20eb65ee681bae493665b2684518a53d0
smoothwizz <a.popa@goit.global>
2023-09-28 15:57:32 +0300

    Update Hermes logo

--- next-client/app/components/Button/Button.component.tsx
+++ next-client/app/components/Button/Button.component.tsx
@@ -27,8 +27,8 @@ const variantStyles = (variant: ButtonVariant) => {
   const baseStyles = `rounded-md leading-tight px-6 py-3  text-white transition-colors ease-in`;
   const disabledStyles = ` disabled:opacity-50 disabled:pointer-events-none`;
 
-  const primaryStyles = `${baseStyles} bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 ${disabledStyles}`;
-  const defaultStyles = `${baseStyles} bg-gray-900 hover:bg-gray-800 focus:bg-gray-800
+  const primaryStyles = `${baseStyles} bg-green-500 hover:bg-green-600 focus:bg-green-600 ${disabledStyles}`;
+  const defaultStyles = `${baseStyles} bg-gray-900 hover:bg-green-800 focus:bg-gray-800
                         dark:bg-slate-700 dark:hover:bg-slate-600 dark:focus:bg-slate-600 ${disabledStyles}`;
   const secondaryStyles = `${baseStyles} bg-slate-800 not:disabled:hover:bg-slate-900 focus:bg-slate-900 ${disabledStyles}`;
 
