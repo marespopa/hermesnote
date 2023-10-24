d2fb1bfd3409bda80427d3c5ebc4a3f8ebcece0c
smoothwizz <hello@marespopa.com>
2023-10-24 20:59:08 +0300

    (feat) Support code highlighting

--- next-client/app/dashboard/editor/EditorPreview.tsx
+++ next-client/app/dashboard/editor/EditorPreview.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nord } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 type Props = {
   content: string;
@@ -9,11 +11,35 @@ type Props = {
 const EditorPreview = ({ content }: Props) => {
   return (
     <div className={previewStyles} id={"pdfExport"}>
-      <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code(props) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                style={nord}
+                language={match[1]}
+                PreTag="div"
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </Markdown>
     </div>
   );
 };
 
-const previewStyles = `w-1/2 prose dark:prose-li:marker:text-gray-300 dark:prose-invert p-4 my-4 rounded-md bg-white dark:bg-slate-700`;
+const previewStyles = `w-1/2 prose dark:prose-li:marker:text-gray-300 dark:prose-invert p-4 my-4 rounded-md bg-white dark:bg-slate-700 prose-pre:bg-gray-100 prose-pre:dark:bg-slate-600 prose-pre:dark:text-white prose-pre:text-gray-700`;
 
 export default EditorPreview;
