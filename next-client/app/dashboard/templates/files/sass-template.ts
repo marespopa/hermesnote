32f64c7e571d94fdcb22d4050c11036e39641a1f
smoothwizz <hello@marespopa.com>
2023-10-25 19:05:05 +0300

    housekeeping: Update templates

---
+++ next-client/app/dashboard/templates/files/sass-template.ts
@@ -0,0 +1,81 @@
+import { MarkdownTemplate } from "..";
+
+const SASSTemplate: MarkdownTemplate = {
+  filename: "sass-documentation.md",
+  frontMatter: {
+    title: "SASS Documentation",
+    description: `With this template, you can efficiently create and organize documentation to ensure your users have a smooth and informed experience with your SaaS product.`,
+    tags: "documentation,sass,software,startup",
+  },
+  content: `
+# [Product Name] Documentation
+
+Welcome to the official documentation for [Product Name]. This comprehensive guide will help you get started with our SaaS product and make the most of its features.
+---
+## 1. Introduction
+
+[Product Name] is a powerful SaaS product designed to [briefly describe the main purpose or value of your product]. This documentation will guide you through the setup, usage, and advanced features of [Product Name].
+
+## 2. Getting Started
+
+### 2.1. Account Setup
+
+Before you can start using [Product Name], you need to set up your account. Follow these steps to get started:
+- [Step 1]
+- [Step 2]
+- [Step 3]
+
+### 2.2. User Registration
+
+Learn how to register and manage users within your [Product Name] account. This section provides instructions on user management and access control.
+
+## 3. Features
+
+[Product Name] offers a range of features to enhance your [specific value] experience. Explore these key features to make the most of the product.
+
+### 3.1. Feature 1
+
+[Detailed description of Feature 1, including how to use it, its benefits, and any relevant settings or options.]
+
+### 3.2. Feature 2
+
+[Detailed description of Feature 2, including how to use it, its benefits, and any relevant settings or options.]
+
+### 3.3. Feature 3
+
+[Detailed description of Feature 3, including how to use it, its benefits, and any relevant settings or options.]
+
+## 4. FAQs
+
+Find answers to frequently asked questions about [Product Name]. If you have questions not covered here, please [link to contact support].
+
+## 5. Troubleshooting
+
+### 5.1. Common Issues
+
+This section provides solutions to common issues users might encounter and tips for resolving them.
+
+### 5.2. Contact Support
+
+If you can't find a solution to your problem in this documentation, please don't hesitate to [contact our support team](mailto:support@example.com) for assistance.
+
+## 6. API Documentation
+
+Developers can refer to our API documentation to integrate [Product Name] into their applications. Learn about available endpoints, authentication, and data formats.
+
+## 7. Terms of Service
+
+Read our [Terms of Service](link-to-terms-of-service) to understand the legal aspects of using [Product Name].
+
+## 8. Privacy Policy
+
+Our [Privacy Policy](link-to-privacy-policy) explains how we collect, use, and protect your data while using [Product Name].
+
+For updates, announcements, and additional resources, visit our [website](https://www.example.com).
+
+Thank you for choosing [Product Name]! We're here to help you make the most of our SaaS product. If you have any feedback or suggestions, please [contact us](mailto:feedback@example.com).
+  
+  `,
+};
+
+export default SASSTemplate;
--- next-client/app/dashboard/templates/index.ts
+++ next-client/app/dashboard/templates/index.ts
@@ -1,10 +1,10 @@
 import { FrontMatterGeneric } from "@/app/types/markdown";
-import FeatureTemplate from "./files/software-feature-task-template";
 import MonthlyBudgetTemplate from "./files/budget-monthly-template";
 import YearlyBudgetTemplate from "./files/budget-yearly-template";
 import ToDoListTemplate from "./files/todo-list-template";
 import ProjectManagementTemplate from "./files/project-management-template";
 import SoftwareTaskTemplate from "./files/software-task-template";
+import SASSTemplate from "./files/sass-template";
 
 export type MarkdownTemplate = {
   filename: string;
@@ -15,8 +15,8 @@ export type MarkdownTemplate = {
 const MarkdownTemplateList = [
   ToDoListTemplate,
   SoftwareTaskTemplate,
-  FeatureTemplate,
   ProjectManagementTemplate,
+  SASSTemplate,
   MonthlyBudgetTemplate,
   YearlyBudgetTemplate,
 ];
