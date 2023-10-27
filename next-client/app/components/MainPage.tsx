d9c724c968a7c6221380d8aab2a5b40a904bb349
smoothwizz <hello@marespopa.com>
2023-10-27 18:13:57 +0300

    (feat): Add different mobile content

---
+++ next-client/app/components/MainPage.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Script from "next/script";
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { useWindowSize } from "../hooks/use-mobile";
+import CookieConsent from "./CookieConsent";
+import CustomProviders from "./CustomProviders";
+import Footer from "./Footer/Footer.component";
+import Header from "./Header";
+import { Montserrat } from "next/font/google";
+const mainFont = Montserrat({ subsets: ["latin"] });
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MainPage = ({ children }: Props) => {
+  const { width: windowWidth } = useWindowSize();
+  const isBrowserMobile = !!windowWidth && windowWidth < 992;
+
+  if (isBrowserMobile) {
+    return (
+      <html lang="en">
+        <body
+          className={`${mainFont.className} max-w-sm prose dark:prose-invert px-4 py-24 bg-white dark:bg-slate-800`}
+        >
+          <main data-testid="landing-page" className="py-4 px-4">
+            <h1>HermesMD</h1>
+            <p>
+              This app is available only on desktop, as the File Api is not
+              available on mobile.
+            </p>
+          </main>
+        </body>
+      </html>
+    );
+  }
+
+  return (
+    <html lang="en">
+      <body className={mainFont.className}>
+        <Toaster position="top-center" reverseOrder={false} />
+        <CustomProviders>
+          <div className="min-h-screen flex flex-col bg-sky-50 dark:bg-slate-800">
+            <Header />
+
+            <div className="flex-1 sm:px-4 md:px-0">{children}</div>
+
+            <Footer />
+          </div>
+        </CustomProviders>
+        <CookieConsent />
+        <Script
+          defer
+          async
+          data-host="hermesmd.netlify.app"
+          src="https://liteanalytics.com/lite.js"
+        ></Script>
+      </body>
+    </html>
+  );
+};
+
+export default MainPage;
--- next-client/app/layout.tsx
+++ next-client/app/layout.tsx
@@ -1,13 +1,5 @@
 import "./globals.scss";
-import { Montserrat } from "next/font/google";
-import CustomProviders from "@/app/components/CustomProviders";
-import Header from "./components/Header";
-import Footer from "./components/Footer/Footer.component";
-import CookieConsent from "./components/CookieConsent";
-import Script from "next/script";
-import { Toaster } from "react-hot-toast";
-
-const mainFont = Montserrat({ subsets: ["latin"] });
+import MainPage from "./components/MainPage";
 
 export const metadata = {
   title: "Hermes Notes",
@@ -19,27 +11,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={mainFont.className}>
-        <Toaster position="top-center" reverseOrder={false} />
-        <CustomProviders>
-          <div className="min-h-screen flex flex-col bg-sky-50 dark:bg-slate-800">
-            <Header />
-
-            <div className="flex-1 sm:px-4 md:px-0">{children}</div>
-
-            <Footer />
-          </div>
-        </CustomProviders>
-        <CookieConsent />
-        <Script
-          defer
-          async
-          data-host="hermesmd.netlify.app"
-          src="https://liteanalytics.com/lite.js"
-        ></Script>
-      </body>
-    </html>
-  );
+  return <MainPage>{children}</MainPage>;
 }
