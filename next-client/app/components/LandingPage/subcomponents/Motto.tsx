d2fe5ea5809220f6a9474f68e6ba93bbbdf08be6
smoothwizz <a.popa@goit.global>
2023-11-01 23:53:20 +0200

    bugfix: Motto margin update

--- next-client/app/components/LandingPage/subcomponents/Motto.tsx
+++ next-client/app/components/LandingPage/subcomponents/Motto.tsx
@@ -10,17 +10,19 @@ export default function Motto() {
 
   return (
     <div className={containerStyle}>
-      <section className="text-center w-1/2 mx-auto">
-        <h3 className="text-2xl mb-4">
+      <section className="text-center w-1/2 mx-auto mb-4">
+        <h3 className="text-2xl">
           Ready to simplify your Markdown workflow and supercharge your
           productivity? Start using Hermes Notes today for effortless Markdown
           editing and PDF exporting.
         </h3>
-        <Button
-          variant="primary-large"
-          label="Get Started!"
-          handler={() => router.push("/dashboard")}
-        ></Button>
+        <div className="my-4">
+          <Button
+            variant="primary-large"
+            label="Get Started!"
+            handler={() => router.push("/dashboard")}
+          ></Button>
+        </div>
       </section>
     </div>
   );
