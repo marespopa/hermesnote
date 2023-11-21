bfbe40f9c8a01f8d3f43d4d5896771d89849af87
smoothwizz <a.popa@goit.global>
2023-11-21 16:27:57 +0200

    feat: Remove header nav tabs

--- next-client/app/dashboard/editor/Editor.tsx
+++ next-client/app/dashboard/editor/Editor.tsx
@@ -3,26 +3,13 @@
 import { useState } from "react";
 import EditorForm from "./EditorForm";
 import EditorHeader from "./EditorHeader";
-import EditorNavigation from "./EditorNavigation";
 import EditorContent from "./EditorContent";
 
-export type EditorTabs = "content" | "frontmatter";
-
 export default function Editor() {
-  const [tab, setTab] = useState<EditorTabs>("content");
-
   return (
     <div className="mb-8">
-      <EditorHeader
-        activeTab={tab}
-        navigate={(selectedTab: EditorTabs) => setTab(selectedTab)}
-      />
-      <EditorNavigation
-        activeTab={tab}
-        handler={(selectedTab: EditorTabs) => setTab(selectedTab)}
-      />
-      {tab === "content" && <EditorContent />}
-      {tab === "frontmatter" && <EditorForm />}
+      <EditorHeader />
+      <EditorContent />
     </div>
   );
 }
