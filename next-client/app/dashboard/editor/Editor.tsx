110f04dbcdf755b68f08132a501d850715459e90
smoothwizz <a.popa@goit.global>
2023-12-03 13:50:52 +0200

    Hide Timer

--- next-client/app/dashboard/editor/Editor.tsx
+++ next-client/app/dashboard/editor/Editor.tsx
@@ -2,14 +2,12 @@
 
 import EditorHeader from "./EditorHeader";
 import EditorContent from "./EditorContent";
-import TimerContainer from "@/app/components/Timer/TimerContainer.component";
 
 export default function Editor() {
   return (
     <div className="mb-8">
       <EditorHeader />
       <EditorContent />
-      <TimerContainer />
     </div>
   );
 }
