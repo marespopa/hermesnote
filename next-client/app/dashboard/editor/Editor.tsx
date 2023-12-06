94989b2dfce84d03c874c7ab636efb92fed5fbcd
smoothwizz <a.popa@goit.global>
2023-12-06 21:39:28 +0200

    chore: show timer only on dev mode

--- next-client/app/dashboard/editor/Editor.tsx
+++ next-client/app/dashboard/editor/Editor.tsx
@@ -5,6 +5,7 @@ import EditorContent from "./EditorContent";
 import Timer from "@/app/components/Timer";
 import { useAtom } from "jotai";
 import { atom_timerSettings } from "@/app/atoms/atoms";
+import { useSearchParams } from "next/navigation";
 
 const DEFAULTS_TIMES = {
   work: 25,
@@ -15,24 +16,28 @@ const DEFAULTS_TIMES = {
 
 export default function Editor() {
   const [timerSettings] = useAtom(atom_timerSettings);
+  const searchParams = useSearchParams();
+  const isTimerVisible = searchParams.has("dev");
 
   return (
     <div className="mb-8">
       <EditorHeader />
       <EditorContent />
-      <Timer
-        settings={{
-          pomodoroTime:
-            timerSettings.workSessionDurationInMin || DEFAULTS_TIMES.work,
-          shortRestTime:
-            timerSettings.shortBreakSessionDurationInMin ||
-            DEFAULTS_TIMES.shortBreak,
-          longRestTime:
-            timerSettings.longBreakSessionDurationInMin ||
-            DEFAULTS_TIMES.longBreak,
-          cycles: DEFAULTS_TIMES.cycles,
-        }}
-      />
+      {isTimerVisible && (
+        <Timer
+          settings={{
+            pomodoroTime:
+              timerSettings.workSessionDurationInMin || DEFAULTS_TIMES.work,
+            shortRestTime:
+              timerSettings.shortBreakSessionDurationInMin ||
+              DEFAULTS_TIMES.shortBreak,
+            longRestTime:
+              timerSettings.longBreakSessionDurationInMin ||
+              DEFAULTS_TIMES.longBreak,
+            cycles: DEFAULTS_TIMES.cycles,
+          }}
+        />
+      )}
     </div>
   );
 }
