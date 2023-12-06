b040f305bf33987bdfc8fc93090ba31035ed9e12
smoothwizz <a.popa@goit.global>
2023-12-06 20:44:47 +0200

    fix: remove router

--- next-client/app/components/Seo.tsx
+++ next-client/app/components/Seo.tsx
@@ -1,7 +1,6 @@
 "use-client";
 
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { ComponentPropsWithoutRef } from "react";
 
 export const defaultMeta = {
@@ -19,7 +18,6 @@ type SeoProps = {
 } & Partial<typeof defaultMeta>;
 
 export default function Seo(props: SeoProps) {
-  const router = useRouter();
   const meta = {
     ...defaultMeta,
     ...props,
@@ -38,8 +36,6 @@ export default function Seo(props: SeoProps) {
       />
       <meta name="robots" content={meta.robots} />
       <meta content={meta.description} name="description" />
-      <meta property="og:url" content={`${meta.url}${router.asPath}`} />
-      <link rel="canonical" href={`${meta.url}${router.asPath}`} />
       {/* Open Graph */}
       <meta property="og:type" content={meta.type} />
       <meta property="og:site_name" content={meta.siteName} />
