ce1bd187ec0152cada525ee0d870f517e277acc1
smoothwizz <a.popa@goit.global>
2023-12-07 12:18:58 +0200

    chore: remove timer

--- next-client/app/dashboard/editor/Editor.tsx
+++ next-client/app/dashboard/editor/Editor.tsx
@@ -4,8 +4,10 @@ import EditorHeader from "./EditorHeader";
 import EditorContent from "./EditorContent";
 import Timer from "@/app/components/Timer";
 import { useAtom } from "jotai";
-import { atom_timerSettings } from "@/app/atoms/atoms";
+import { atom_frontMatter, atom_timerSettings } from "@/app/atoms/atoms";
 import { useSearchParams } from "next/navigation";
+import { useDocumentTitle } from "@/app/hooks/use-document-title";
+import { useEffect } from "react";
 
 const DEFAULTS_TIMES = {
   work: 25,
@@ -18,26 +20,18 @@ export default function Editor() {
   const [timerSettings] = useAtom(atom_timerSettings);
   const searchParams = useSearchParams();
   const isTimerVisible = searchParams.has("dev");
+  const [frontMatter] = useAtom(atom_frontMatter);
+  const fileTitle = frontMatter.title || "File";
+  const [_, setDocumentTitle] = useDocumentTitle("Hermes Notes");
+
+  useEffect(() => {
+    setDocumentTitle(fileTitle);
+  }, [fileTitle, setDocumentTitle]);
 
   return (
     <div className="mb-8">
       <EditorHeader />
       <EditorContent />
-      {isTimerVisible && (
-        <Timer
-          settings={{
-            pomodoroTime:
-              timerSettings.workSessionDurationInMin || DEFAULTS_TIMES.work,
-            shortRestTime:
-              timerSettings.shortBreakSessionDurationInMin ||
-              DEFAULTS_TIMES.shortBreak,
-            longRestTime:
-              timerSettings.longBreakSessionDurationInMin ||
-              DEFAULTS_TIMES.longBreak,
-            cycles: DEFAULTS_TIMES.cycles,
-          }}
-        />
-      )}
     </div>
   );
 }
