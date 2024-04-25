b9ccd801aeffcd41d2ea9bb014d26cd2ce338fcc
smoothwizz <a.popa@goit.global>
2024-04-25 18:52:28 +0300

    fix: add suspense in editor page

--- next-client/app/dashboard/editor/page.tsx
+++ next-client/app/dashboard/editor/page.tsx
@@ -1,10 +1,18 @@
 import { containerStyle } from "@/app/constants/styles";
 import Editor from "./Editor";
+import { Suspense } from "react";
+import Loading from "@/app/components/Loading";
 
 export default function EditorPage() {
+  function SearchBarFallback() {
+    return <Loading message="Getting things ready..." />;
+  }
+
   return (
     <div className={`${containerStyle} px-2 md:px-0`}>
-      <Editor />
+      <Suspense fallback={<SearchBarFallback />}>
+        <Editor />
+      </Suspense>
     </div>
   );
 }
