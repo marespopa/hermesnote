c0b8b7a8e80452a3f167e3f6828db897cbc3739e
Mares Popa <popa.mares@gmail.com>
2024-11-08 19:11:17 +0200

    fix: add image loader

--- next-client/app/components/Footer/components/ProductHuntBadge.tsx
+++ next-client/app/components/Footer/components/ProductHuntBadge.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import React from "react";
 
 export default function ProductHuntBadge() {
+  const badgeLoader = () => {
+    return `https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=377056&theme=dark`
+  }
+  
   return (
     <div className="flex justify-center items-start mt-2 sm:mt-0">
       <a
@@ -9,6 +13,7 @@ export default function ProductHuntBadge() {
         target="_blank"
       >
         <Image
+          loader={badgeLoader}
           src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=587597&theme=neutral"
           alt="Hermes&#0032;Markdown - Write&#0044;&#0032;Edit&#0044;&#0032;and&#0032;Export&#0032;Markdown&#0046;&#0032;Your&#0032;Way&#0046; | Product Hunt"
           style={{ width: "250px", height: "54px" }}
