3a12e52d76a20847ebd87f71c55860c1a2653673
Mares Popa <popa.mares@gmail.com>
2024-11-08 18:42:41 +0200

    chore: update yarn version; content: update pricing page;

--- next-client/app/pricing/page.tsx
+++ next-client/app/pricing/page.tsx
@@ -5,20 +5,16 @@ export default function Prices() {
     <div className="w-full max-w-screen-xl mx-auto my-4 md:my-8">
       <section className="px-4 prose dark:prose-invert">
         <h2 className="text-xl">
-          HermesNotes is <strong>100% free to use</strong>.
+          Hermes Markdown is <strong>100% free to use</strong>.
         </h2>
         <p>
-          At HermesNotes, our mission is to make Markdown accessible and
-          straightforward for everyone. HermesNotes is designed as a completely
-          free tool, created to help you work with Markdown effortlessly,
-          without any cost.
+          HermesNotes is designed as a completely free tool, created to help you
+          work with Markdown effortlessly, without any cost.
         </p>
 
         <p>
-          Having used HermesNotes extensively myself to streamline my Markdown
-          workflow, I&apos;m excited to share this tool with you. HermesNotes
-          will always be free, reflecting our commitment to making Markdown
-          simple and accessible to all.
+          Having used HermesMarkdown extensively myself to streamline my
+          Markdown workflow, I&apos;m excited to share this tool with you.
         </p>
       </section>
     </div>
